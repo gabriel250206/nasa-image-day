d7492573831e0b932a8487da7f2dc049eb7ca2e0
gabriel250206 <158100593+gabriel250206@users.noreply.github.com>
2025-01-28 08:27:29 -0600

    primer commit

---
+++ nasa-image-of-the-day/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nasa-image-of-the-day',
+  webDir: 'www'
+};
+
+export default config;
